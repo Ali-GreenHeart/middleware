c278eb461329d49c5ebefa2536f77b7155afc40a
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2024-04-16 11:11:47 +0400

    rename schemat to model

--- mongoose/index.js
+++ mongoose/index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import mongoose from 'mongoose'
-import bookModel from './schema/book.js'
+import bookModel from './model/book.js'
 
 
 const connectionString = `mongodb+srv://node02:node02@cluster0.twl8ykw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
