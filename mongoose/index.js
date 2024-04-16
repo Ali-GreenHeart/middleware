3df2f83b20d9499cfd1416e1b5552f3e217f368d
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2024-04-16 11:11:00 +0400

    add simple routes

--- mongoose/index.js
+++ mongoose/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import bookModel from './schema/book.js'
 
+
 const connectionString = `mongodb+srv://node02:node02@cluster0.twl8ykw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 mongoose.connect(connectionString)
     .then((value) => {
@@ -9,13 +10,30 @@ mongoose.connect(connectionString)
     })
 
 const app = express()
+app.use(express.json())
 
 
 
-app.get('/', (req, res) => {
-
+app.get('/', async (req, res) => {
+    const a = await bookModel.find()
+    res.send(a)
 })
-
+app.post('/', async (req, res) => {
+    const a = await bookModel.create(req.body)
+    res.send(a)
+})
+app.put('/:id', async (req, res) => {
+    const id = req.params.id
+    const a = await bookModel.findByIdAndUpdate(
+        id,
+        req.body,
+        {
+            new: true
+        }
+    )
+    res.send(a)
+})
+// delete ozunuz yazin !
 app.listen(3457, () => {
     console.log('server is up')
 })
