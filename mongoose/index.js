c2738c1890ea0eda06ef0d0697efc510910901da
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2024-04-30 10:54:00 +0400

    add login

--- mongoose/index.js
+++ mongoose/index.js
@@ -1,35 +1,40 @@
 import express from 'express'
-import bookModel from './model/book.js'
 import connectDb from './connect.js'
+import userModel from './model/userModel.js'
+import bcrypt from 'bcrypt'
 
 connectDb()
-
-
 const app = express()
 app.use(express.json())
 
-
-
-app.get('/', async (req, res) => {
-    const a = await bookModel.find()
-    res.send(a)
+app.post('/register', async (req, res) => {
+    let password = await bcrypt.hash(req.body.password, 10)
+    await userModel.create({ ...req.body, password })
+    res.send("User has been created!")
 })
-app.post('/', async (req, res) => {
-    const a = await bookModel.create(req.body)
-    res.send(a)
-})
-app.put('/:id', async (req, res) => {
-    const id = req.params.id
-    const a = await bookModel.findByIdAndUpdate(
-        id,
-        req.body,
-        {
-            new: true
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body
+    const { _doc: { password: passwordInDb, ...frontUser } } = await userModel.findOne({ username })
+    if (passwordInDb) {
+        const isPwdTrue = await bcrypt.compare(password, passwordInDb)
+        if (isPwdTrue) {
+            res.send(frontUser)
+        } else {
+            res.status(401).send('Password is wrong!')
         }
-    )
-    res.send(a)
+    } else {
+        res.status(404).send("user doesn't exist")
+    }
 })
-// delete ozunuz yazin !
+
+app.get('/profile', async (req, res) => {
+    // 1. adam login etmis olmalidir!
+    // 2. ozunun profile-melumatlarini gormelidir!
+    // JWT!
+})
+
 app.listen(3457, () => {
     console.log('server is up')
 })
+
