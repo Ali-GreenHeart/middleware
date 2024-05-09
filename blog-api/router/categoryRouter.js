6b4a0ed99da842b6e501475153950f045dc8f0ac
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2024-05-09 10:26:42 +0400

    fix wrong-function

--- blog-api/router/categoryRouter.js
+++ blog-api/router/categoryRouter.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createCategory, deleteCategory, getCategories } from "../service/categoryService.js";
+import { createCategory, deleteCategory, editCategory, getCategories } from "../service/categoryService.js";
 
 const categRouter = new Router()
 
@@ -12,7 +12,7 @@ categRouter.post("/", async (req, res) => {
     res.status(200).send(msg)
 })
 categRouter.put("/:id", async (req, res) => {
-    const msg = await createCategory(req.params.id, req.body)
+    const msg = await editCategory(req.params.id, req.body)
     res.status(200).send(msg)
 })
 categRouter.delete("/:id", async (req, res) => {
